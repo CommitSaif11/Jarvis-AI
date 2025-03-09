ba3ef27bad35259aa84441b14abfc6d3a88e9761
Saif <alamsaif1107@gmail.com>
2025-03-09 19:26:27 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -102,23 +102,22 @@ if __name__ == "__main__":
             speak(f"Sir, the time is {strTime}")
 
         elif 'open code' in query:
-            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            codePath = "C:\\Users\\Saif\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
             os.startfile(codePath)
 
-        elif 'email to harry' in query:
+        elif 'email to saif' in query:
             try:
                 speak("What should I say?")
                 content = takeCommand()
-                to = "harryyourEmail@gmail.com"    
+                to = "saifyourEmail@gmail.com"    
                 sendEmail(to, content)
                 speak("Email has been sent!")
             except Exception as e:
                 print(e)
-                speak("Sorry my friend harry bhai. I am not able to send this email")
+                speak("Sorry my friend saify bhai. I am not able to send this email")
 
 
-        elif 'open' in query:
-            webbrowser.open("xhamster.com")  
+        
 
         
 
